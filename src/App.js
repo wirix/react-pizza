a1da296269d1cb3e21a1b4609a381151c18f761e
wirix <88577048+wirix@users.noreply.github.com>
2022-08-15 10:01:39 +0700

    added design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Categories from './components/Categories/Categories'
+import Header from './components/Header/Header'
+import PizzaBlock from './components/PizzaBlock/PizzaBlock'
+import Sort from './components/Sort/Sort'
+import './scss/app.scss'
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      test
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
