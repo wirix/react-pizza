a65ee3a50a0ce7a26280243dd62baac6f8d4e4ef
wirix <88577048+wirix@users.noreply.github.com>
2022-08-16 17:11:41 +0700

    lsLoaderPizza

--- src/App.js
+++ src/App.js
@@ -4,9 +4,23 @@ import Header from './components/Header/Header'
 import PizzaBlock from './components/PizzaBlock/PizzaBlock'
 import Sort from './components/Sort/Sort'
 import './scss/app.scss'
-import pizzas from './assets/pizzas.json'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import Skeleton from './components/PizzaBlock/Skeleton'
 
 function App(props) {
+  const [items, setItems] = useState([])
+  // const [itemsSkeleton, setItemsSkeleton] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+  const [isLoaderPizza, setIsLoaderPizza] = useState(true)
+
+  useEffect(() => {
+    axios.get(`https://62fb49efe4bcaf535180e06c.mockapi.io/items`)
+    .then(response => {
+      setItems(response.data)
+      setIsLoaderPizza(false)
+    })
+  }, [])
+  
   return (
     <div className="wrapper">
       <Header />
@@ -18,9 +32,12 @@ function App(props) {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map(p => (
-              <PizzaBlock {...p} key={p.id} />
-            ))}
+            {
+              isLoaderPizza ? [...new Array(8)].map((_, index) => <Skeleton key={index} />)
+                : items.map(i => (
+                  <PizzaBlock {...i} key={i.id} />
+                ))
+            }
           </div>
         </div>
       </div>
